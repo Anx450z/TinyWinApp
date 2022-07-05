30607eaf98dbabe2891c07044cc23ce926191582
Anx450z <anx450z@gmail.com>
2022-07-06 00:02:08 +0530

    Add test and update layout of static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,31 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = " | TinyWin"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home#{@base_title}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help#{@base_title}"
   end
 
   test "should get privacy" do
     get static_pages_privacy_url
     assert_response :success
+    assert_select "title", "Privacy#{@base_title}"
   end
 
   test "should get tos" do
     get static_pages_tos_url
     assert_response :success
+    assert_select "title", "TOS#{@base_title}"
   end
 end
