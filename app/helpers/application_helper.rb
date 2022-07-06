cc918320e56794506d23815d58b4210eca679bbc
Anx450z <anx450z@gmail.com>
2022-07-06 23:26:19 +0530

    Update to static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return the full title on per - page basis
+  def full_title(page_title = " ")
+    base_title = "TinyWin"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
